da72d01b3b4b4298a8945348bcf5833b9c648748
OguzBulun <oguzbulun@hotmail.com>
2021-03-09 20:10:18 +0300

    Main

--- SportotoSuperLig/app/src/main/java/com/example/sportotosuperlig/dbRoom/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.sportotosuperlig.dbRoom;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Team.class}, version = 2)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract TeamDao teamDao();
-}
